a3f86c8157ac6a8294d2d5a6c11d6ae072f05745
ktan1@stevens.edu <ktan1@stevens.edu>
2020-04-16 13:54:54 -0400

    beginning work with tkinter

--- bmicalculator.py
+++ bmicalculator.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import tkinter as tk
 
 feet = 5
 inches = 6
@@ -34,4 +35,32 @@ def provide_recs(bmi):
                     print(''.join([i for i in elem.div.a.text if not i.isdigit()])[2:] )
 
 
-provide_recs(15)
+# provide_recs(15)
+
+HEIGHT = 500
+WIDTH = 600
+
+def test_function(entry):
+    print("This is the entry: ", entry)
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+frame = tk.Frame(root, bg='#80c1ff', bd=5)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+
+entry = tk.Entry(frame, font=40)
+entry.place(relwidth=0.65, relheight=1)
+
+button = tk.Button(frame, text='Calculate', font=40, command=lambda: test_function(entry.get()))
+button.place(relx=0.7, relwidth=0.3, relheight=1)
+
+lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth = 0.75, relheight=0.6, anchor='n')
+
+label = tk.Label(lower_frame)
+label.place(relwidth=1, relheight=1)
+
+root.mainloop()
