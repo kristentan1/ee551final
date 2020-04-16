61b51d278c58408e25adef19f90d1e94b9ef0709
ktan1@stevens.edu <ktan1@stevens.edu>
2020-04-16 00:30:18 -0400

    worked on web scraping for > 24.9

--- bmicalculator.py
+++ bmicalculator.py
@@ -21,10 +21,21 @@ def provide_recs(bmi):
     elif(18.5 <= bmi < 25):
         print('Healthy Weight')
     else:
-        page = requests.get('https://www.healthline.com/nutrition/20-most-weight-loss-friendly-foods#section11')
-        soup = BeautifulSoup(page.content, 'lxml')
-        # match = soup.find('div', class_='css-0').h2.div.a.text
-        match = soup.find_all('article', class_ = 'article-body css-d2znx6 undefined')
-        print(match)
+        # page = requests.get('https://www.healthline.com/nutrition/20-most-weight-loss-friendly-foods#section11')
+        # soup = BeautifulSoup(page.content, 'lxml')
+        # # match = soup.find('div', class_='css-0').h2.div.a.text
+        # match = soup.find_all('article', class_ = 'article-body css-d2znx6 undefined')
+        # print(match)
+
+        # ATTEMPTING WITH DIFF LINK
+        page = requests.get('https://www.eatthis.com/healthy-weight-loss-foods/')
+        soup = BeautifulSoup(page.content,'lxml')
+        # match = soup.find('div', class_ = 'main-content').find('div', class_='content').find('div', class_='number-head-mod number-head-mod-standalone').find('div', class_='header-mod').h2.text
+        match = soup.find('div', class_ = 'main-content').find('div', class_='content').find_all('h2', class_='title')
+        for elem in match:
+            print(elem.text)
+        # file = open('testfile2.txt','w')
+        # file.write(str(match))
+
 
 provide_recs(28)
