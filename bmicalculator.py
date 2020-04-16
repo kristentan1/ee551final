e48a2f4ec6b436b5a4fc2b039a06ea3aa072b32c
ktan1@stevens.edu <ktan1@stevens.edu>
2020-04-16 14:31:30 -0400

    Integrated calculate function with tkinter button

--- bmicalculator.py
+++ bmicalculator.py
@@ -6,12 +6,16 @@ feet = 5
 inches = 6
 weight = 140
 
-def calculate_bmi(feet, inches, weight):
+def calculate_bmi(measurements):
+    feet = int(measurements[0])
+    inches = int(measurements[1])
+    weight = int(measurements[2])
     total_height_meters =((feet * 12) + inches) * 0.0254
     weight_kilograms = weight * 0.45359237
-    return round((weight_kilograms / (total_height_meters**2)), 1)
+    # return round((weight_kilograms / (total_height_meters**2)), 1)
+    print(round((weight_kilograms / (total_height_meters**2)), 1))
 
-temp_bmi = calculate_bmi(feet, inches, weight)
+# temp_bmi = calculate_bmi(feet, inches, weight)
 
 def provide_recs(bmi):
     if (bmi < 18.5):
@@ -49,16 +53,22 @@ canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
 frame = tk.Frame(root, bg='#80c1ff', bd=5)
-frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+frame.place(relx=0.5, rely=0.05, relwidth=0.75, relheight=0.2, anchor='n')
 
-entry = tk.Entry(frame, font=40)
-entry.place(relwidth=0.65, relheight=1)
+feet_entry = tk.Entry(frame, font=40,borderwidth=2)
+feet_entry.place(relx=0, rely=0, relwidth=0.3, relheight=0.45)
 
-button = tk.Button(frame, text='Calculate', font=40, command=lambda: test_function(entry.get()))
-button.place(relx=0.7, relwidth=0.3, relheight=1)
+inches_entry = tk.Entry(frame, font=40,borderwidth=2)
+inches_entry.place(relx=0.35, rely=0, relwidth=0.3, relheight=0.45)
+
+weight_entry = tk.Entry(frame, font=40, borderwidth=2)
+weight_entry.place(rely=0.5, relwidth=0.65, relheight=0.45)
+
+button = tk.Button(frame, text='Calculate', font=40, command=lambda: calculate_bmi([feet_entry.get(), inches_entry.get(), weight_entry.get()]))
+button.place(relx=0.7, rely=0.2, relwidth=0.3, relheight=0.6)
 
 lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
-lower_frame.place(relx=0.5, rely=0.25, relwidth = 0.75, relheight=0.6, anchor='n')
+lower_frame.place(relx=0.5, rely=0.3, relwidth = 0.75, relheight=0.65, anchor='n')
 
 label = tk.Label(lower_frame)
 label.place(relwidth=1, relheight=1)
