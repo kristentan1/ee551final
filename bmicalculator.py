5997daf5b7aec792f17af7aac0d46c5f8ba32728
ktan1@stevens.edu <ktan1@stevens.edu>
2020-04-17 12:46:15 -0400

    Added entry field placeholder class and integrated w display

--- bmicalculator.py
+++ bmicalculator.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from tkinter import font
 import tkinter as tk
+import customentry
 
 # Constants
 HEIGHT = 500
@@ -72,22 +73,25 @@ canvas.pack()
 frame = tk.Frame(root, bg='#80c1ff', bd=5)
 frame.place(relx=0.5, rely=0.05, relwidth=0.75, relheight=0.2, anchor='n')
 
-feet_entry = tk.Entry(frame, font=('Courier', 18),borderwidth=2)
+# feet_entry = tk.Entry(frame, font=('Courier', 18),borderwidth=2)
+# feet_entry.place(relx=0, rely=0, relwidth=0.3, relheight=0.45)
+
+feet_entry = customentry.CustomEntry(frame, 'Feet')
 feet_entry.place(relx=0, rely=0, relwidth=0.3, relheight=0.45)
 
-inches_entry = tk.Entry(frame, font=('Courier', 18),borderwidth=2)
+inches_entry = customentry.CustomEntry(frame, 'Inches')
 inches_entry.place(relx=0.35, rely=0, relwidth=0.3, relheight=0.45)
 
-weight_entry = tk.Entry(frame, font=('Courier', 18), borderwidth=2)
+weight_entry = customentry.CustomEntry(frame, 'Pounds')
 weight_entry.place(rely=0.5, relwidth=0.65, relheight=0.45)
 
-button = tk.Button(frame, text='Calculate', font=('Courier', 12), command=lambda: calculate_bmi([feet_entry.get(), inches_entry.get(), weight_entry.get()]))
+button = tk.Button(frame, text='Calculate', font=('Arial', 12), command=lambda: calculate_bmi([feet_entry.get(), inches_entry.get(), weight_entry.get()]))
 button.place(relx=0.7, rely=0.2, relwidth=0.3, relheight=0.6)
 
 lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
 lower_frame.place(relx=0.5, rely=0.3, relwidth = 0.75, relheight=0.65, anchor='n')
 
-label = tk.Label(lower_frame, font=('Courier', 12))
+label = tk.Label(lower_frame, font=('Arial', 12))
 label.place(relwidth=1, relheight=1)
 
 root.mainloop()
