c4f5c3f5a9bcfedbbb25b0cfd11434e70932abb7
ktan1@stevens.edu <ktan1@stevens.edu>
2020-05-02 21:35:04 -0400

    fixed formatting

--- customentry.py
+++ customentry.py
@@ -2,10 +2,11 @@
 
 import tkinter as tk
 
+
 class CustomEntry(tk.Entry):
     def __init__(self, master=None, placeholder="PLACEHOLDER", color='grey'):
         super().__init__(master)
-        
+
         self.placeholder = placeholder
         self.placeholder_color = color
         self.default_fg_color = self['fg']
@@ -23,7 +24,7 @@ class CustomEntry(tk.Entry):
         if self['fg'] == self.placeholder_color:
             self.delete('0', 'end')
             self['fg'] = self.default_fg_color
-    
+
     def foc_out(self, *args):
         if not self.get():
             self.put_placeholder()
